9e16e400c6567601a7b95028165d42b7e6f77cf6
iskunalnayyar <iskunalnayyar@gmail.com>
2020-01-20 17:25:40 -0500

    Initial Messing around with Microsoft data. Still has 23 columns that have confusing datatype. Fix the confusing datatype by specifying manually the datatype.

---
+++ WorkinWithData.py
@@ -0,0 +1,33 @@
+"""
+Messing with Data file
+"""
+
+import numpy as np
+import pandas as pd
+import os
+
+
+class MessingWithData:
+
+    def __init__(self, dir, filename):
+        self.dir = dir  # directory
+        self.file = filename  # filename
+
+    def read_file(self):
+        file_1 = pd.read_csv(os.path.join(self.dir, self.file))
+        # (8921483, 83) for train
+        # (7853253, 82) for test
+        print(file_1.shape)
+        print(file_1.size)
+        file_1.fillna(file_1.median())
+        print(file_1)
+        print(file_1.describe())
+
+
+def main():
+    m1 = MessingWithData('/Users/k.n./Downloads/microsoft-malware-prediction', 'test.csv')
+    m1.read_file()
+
+
+if __name__ == '__main__':
+    main()
